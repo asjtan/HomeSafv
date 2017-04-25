7812fee3e79eb02b451bf0874526789dba88dcea
alex <alex_tan@nec.com.sg>
2017-04-25 21:59:50 +0800

    updated the UI for login

---
+++ HomeSafv/LoginVC.swift
@@ -0,0 +1,218 @@
+//
+//  ViewController.swift
+//  HomeSafv
+//
+//  Created by Alex Tan on 25/4/17.
+//  Copyright © 2017 SG4207. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class LoginVC: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @IBOutlet var registerView: UIView!
+    @IBOutlet var loginView: UIView!
+    @IBOutlet weak var darkView: UIView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var profilePic: RoundedImageView!
+    @IBOutlet weak var registerName: UITextField!
+    @IBOutlet weak var registerEmail: UITextField!
+    @IBOutlet weak var registerPassword: UITextField!
+    @IBOutlet var warningLabels: [UILabel]!
+    @IBOutlet weak var loginEmail: UITextField!
+    @IBOutlet weak var loginPassword: UITextField!
+    @IBOutlet var inputFields: [UITextField]!
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var createLabel: UIButton!
+    var loginViewTopConstraint: NSLayoutConstraint!
+    var registerTopConstraint: NSLayoutConstraint!
+    let imagePicker = UIImagePickerController()
+    var isLoginViewVisible = true
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        get {
+            return UIInterfaceOrientationMask.portrait
+        }
+    }
+    
+    func customization()  {
+        self.darkView.alpha = 0
+        self.imagePicker.delegate = self
+        self.profilePic.layer.borderColor = GlobalVariables.blue.cgColor
+        self.profilePic.layer.borderWidth = 2
+        
+        self.view.insertSubview(self.loginView, belowSubview: self.createLabel)
+        self.loginView.translatesAutoresizingMaskIntoConstraints = false
+        self.loginView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.loginViewTopConstraint = NSLayoutConstraint.init(item: self.loginView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 60)
+        self.loginViewTopConstraint.isActive = true
+        self.loginView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.45).isActive = true
+        self.loginView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
+        self.loginView.layer.cornerRadius = 8
+        
+        self.view.insertSubview(self.registerView, belowSubview: self.createLabel)
+        self.registerView.translatesAutoresizingMaskIntoConstraints = false
+        self.registerView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.registerTopConstraint = NSLayoutConstraint.init(item: self.registerView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 1000)
+        self.registerTopConstraint.isActive = true
+        self.registerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.6).isActive = true
+        self.registerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
+        self.registerView.layer.cornerRadius = 8
+    }
+    
+    func showLoading(state: Bool)  {
+        if state {
+            self.darkView.isHidden = false
+            self.spinner.startAnimating()
+            UIView.animate(withDuration: 0.3, animations: {
+                self.darkView.alpha = 0.5
+            })
+        } else {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.darkView.alpha = 0
+            }, completion: { _ in
+                self.spinner.stopAnimating()
+                self.darkView.isHidden = true
+            })
+        }
+    }
+    
+    func pushTomainView() {
+        /*let vc = self.storyboard?.instantiateViewController(withIdentifier: "Navigation") as! NavVC
+        self.show(vc, sender: nil)*/
+    }
+    
+    func openPhotoPickerWith(source: PhotoSource) {
+        switch source {
+        case .camera:
+            let status = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+            if (status == .authorized || status == .notDetermined) {
+                self.imagePicker.sourceType = .camera
+                self.imagePicker.allowsEditing = true
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }
+        case .library:
+            let status = PHPhotoLibrary.authorizationStatus()
+            if (status == .authorized || status == .notDetermined) {
+                self.imagePicker.sourceType = .savedPhotosAlbum
+                self.imagePicker.allowsEditing = true
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    @IBAction func switchViews(_ sender: UIButton) {
+        if self.isLoginViewVisible {
+            self.isLoginViewVisible = false
+            sender.setTitle("Login", for: .normal)
+            self.loginViewTopConstraint.constant = 1000
+            self.registerTopConstraint.constant = 60
+        } else {
+            self.isLoginViewVisible = true
+            sender.setTitle("Create New Account", for: .normal)
+            self.loginViewTopConstraint.constant = 60
+            self.registerTopConstraint.constant = 1000
+        }
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        })
+        for item in self.warningLabels {
+            item.isHidden = true
+        }
+    }
+    
+    @IBAction func register(_ sender: Any) {
+        for item in self.inputFields {
+            item.resignFirstResponder()
+        }
+        self.showLoading(state: true)
+        /*User.registerUser(withName: self.registerNameField.text!, email: self.registerEmailField.text!, password: self.registerPasswordField.text!, profilePic: self.profilePicView.image!) { [weak weakSelf = self] (status) in
+            DispatchQueue.main.async {
+                weakSelf?.showLoading(state: false)
+                for item in self.inputFields {
+                    item.text = ""
+                }
+                if status == true {
+                    weakSelf?.pushTomainView()
+                    weakSelf?.profilePicView.image = UIImage.init(named: "profile pic")
+                } else {
+                    for item in (weakSelf?.waringLabels)! {
+                        item.isHidden = false
+                    }
+                }
+            }
+        }*/
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        for item in self.inputFields {
+            item.resignFirstResponder()
+        }
+        self.showLoading(state: true)
+        /*User.loginUser(withEmail: self.loginEmailField.text!, password: self.loginPasswordField.text!) { [weak weakSelf = self](status) in
+            DispatchQueue.main.async {
+                weakSelf?.showLoading(state: false)
+                for item in self.inputFields {
+                    item.text = ""
+                }
+                if status == true {
+                    weakSelf?.pushTomainView()
+                } else {
+                    for item in (weakSelf?.waringLabels)! {
+                        item.isHidden = false
+                    }
+                }
+                weakSelf = nil
+            }
+        }*/
+    }
+    
+    @IBAction func selectPic(_ sender: Any) {
+        let sheet = UIAlertController(title: nil, message: "Select the source", preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            self.openPhotoPickerWith(source: .camera)
+        })
+        let photoAction = UIAlertAction(title: "Gallery", style: .default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            self.openPhotoPickerWith(source: .library)
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        sheet.addAction(cameraAction)
+        sheet.addAction(photoAction)
+        sheet.addAction(cancelAction)
+        self.present(sheet, animated: true, completion: nil)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        for item in self.warningLabels {
+            item.isHidden = true
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            self.profilePic.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.customization()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.background.layer.removeAllAnimations()
+        self.view.layoutIfNeeded()
+    }
+
+
+}
+
