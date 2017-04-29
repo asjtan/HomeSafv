1db0bbb65bda3f89510ca083778e458988c1d27e
alex <alex_tan@nec.com.sg>
2017-04-29 15:19:08 +0800

    updated images

--- HomeSafv/Generic.swift
+++ HomeSafv/Generic.swift
@@ -13,6 +13,7 @@ import UIKit
 struct GlobalVariables {
     static let blue = UIColor.rbg(r: 129, g: 144, b: 255)
     static let purple = UIColor.rbg(r: 161, g: 114, b: 255)
+    static let orange = UIColor.rbg(r: 255, g: 178, b: 120)
 }
 
 //Extensions
